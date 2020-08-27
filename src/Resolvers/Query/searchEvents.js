47f985e91a2f8679df3f4e86effc03edc07d0eb4
johir-rayhan <johir.rayhan01@gmail.com>
2020-08-27 20:21:08 +0600

    Create a custom graphql query

--- src/Resolvers/Query/searchEvents.js
+++ src/Resolvers/Query/searchEvents.js
@@ -1,4 +1,3 @@
-
 const searchEvents = {
  async searchEventsWithText(parent, { searchText }, ctx, info) {
   return ctx.db.query.events({ where: { title_contains: searchText } });
--- src/Resolvers/resolvers.js
+++ src/Resolvers/resolvers.js
@@ -1,8 +1,10 @@
 import {Query} from './Query'
+import {searchEvents} from './Query/searchEvents'
 
 export const resolvers = {
     Query: {
         ...Query,
+        ...searchEvents
     },
     Mutation: {
     },
